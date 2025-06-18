3c9b906f1a9705d9fd767f88bec7e8c13b23f56e
Arti Thakur <arti31thakur@gmail.com>
2025-06-18 13:51:52 +0530

    Code

--- frontend/Task-Manager/src/pages/Auth/Login.jsx
+++ frontend/Task-Manager/src/pages/Auth/Login.jsx
@@ -1,8 +1,27 @@
 import react from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
-    return(
-        <div>Login</div>
-    )
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    //handle login form submit
+    const handleLogin = async (e) => {
+        e.preventDefault();
+    };
+
+    return <AuthLayout>
+        <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+            Please enter your details to log in
+        </p>
+        </div>
+    </AuthLayout>;
 }
 export default Login;
