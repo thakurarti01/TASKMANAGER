15c5cfe5bc27862609fd3ee03b6d6c319613b4fa
Arti Thakur <arti31thakur@gmail.com>
2025-06-20 00:12:22 +0530

    Code

--- frontend/Task-Manager/src/components/layouts/DashboardLayout.jsx
+++ frontend/Task-Manager/src/components/layouts/DashboardLayout.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+import Navbar from "./Navbar";
+import SideMenu from "./SideMenu";
 
 const DashboardLayout = ({children, activeMenu}) => {
-    return (
-        <div>DashboardLayout</div>
-    )
-}
+    const {user} = useContext(UserContext);
 
-export default DashboardLayout
+    return (
+        <div className="">
+            <Navbar activeMenu={activeMenu} />
+
+            {user && (
+                <div className="flex">
+                    <div className="max-[1080px]:hidden">
+                        <SideMenu activeMenu={activeMenu} />
+                    </div>
+
+                    <div className="grow mx-5">{children}</div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default DashboardLayout;
--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -1,14 +1,42 @@
-import react, { useContext } from "react";
+import react, { useContext, useEffect, useState } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Dashboard = () => {
     useUserAuth();
 
-    const {user} = useContext(UserContext)
+    const {user} = useContext(UserContext);
 
-    return <DashboardLayout>Dashboard</DashboardLayout>;
+    const navigate = useNavigate();
+
+    const [dashboardData, setDashboardData] = useState(null);
+    const [pieChartData, setPieChartData] = useState([]);
+    const [barChartData, setBarChartData] = useState([]);
+
+    const getDashboardData = async () => {
+        try {
+            const response = await axiosInstance.get(
+                API_PATHS.TASKS.GET_DASHBOARD_DATA
+            );
+            if (response.data) {
+                setDashboardData(response.data);
+            }
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
+    };
+
+    useEffect(() => {
+        getDashboardData();
+
+        return () => {};
+    }, []);
+ 
+    return <DashboardLayout activeMenu="Dashboard">Dashboard</DashboardLayout>;
     
 };
 export default Dashboard;
