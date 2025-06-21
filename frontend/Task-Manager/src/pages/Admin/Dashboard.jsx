e4764bf6a9753ba8aaba065732c7614339325eed
Arti Thakur <arti31thakur@gmail.com>
2025-06-21 23:45:21 +0530

    Code

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import moment from 'moment'
 
 const Dashboard = () => {
     useUserAuth();
@@ -37,7 +38,16 @@ const Dashboard = () => {
     }, []);
  
     return <DashboardLayout activeMenu="Dashboard">
-        {JSON.stringify(dashboardData)}
+        <div className="card my-5">
+            <div>
+                <div className="col-span-3">
+                    <h2 className="text-xl md:text-2xl">Good Morning! {user?.name}</h2>
+                    <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
+                        {moment().format("dddd Do MMM YYYY")}
+                    </p>
+                </div>
+            </div>
+        </div>
     </DashboardLayout>;
     
 };
