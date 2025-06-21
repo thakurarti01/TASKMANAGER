367208932ba03b52de0aae2324511eb7afcc9620
Arti Thakur <arti31thakur@gmail.com>
2025-06-22 01:46:16 +0530

    Code

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -5,7 +5,12 @@ import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
-import moment from 'moment'
+import moment from 'moment';
+import { addThousandsSeparator } from "../../utils/helper";
+import InfoCard from "../../components/cards/InfoCard";
+import { LuArrowRight } from "react-icons/lu";
+import TaskListTable from "../../components/TaskListTable";
+
 
 const Dashboard = () => {
     useUserAuth();
@@ -23,6 +28,9 @@ const Dashboard = () => {
             const response = await axiosInstance.get(
                 API_PATHS.TASKS.GET_DASHBOARD_DATA
             );
+
+            // ✅ Add this log here
+        console.log("✅ Dashboard data from API:", response.data);
             if (response.data) {
                 setDashboardData(response.data);
             }
@@ -30,6 +38,10 @@ const Dashboard = () => {
             console.error("Error fetching users:", error);
         }
     };
+
+    const onSeeMore = () => {
+        navigate('/admin/tasks')
+    }
 
     useEffect(() => {
         getDashboardData();
@@ -45,6 +57,56 @@ const Dashboard = () => {
                     <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
                         {moment().format("dddd Do MMM YYYY")}
                     </p>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+                <InfoCard
+                label="Total Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.All || 0
+                )}
+                color="bg-primary"
+                />
+
+                <InfoCard
+                label="Pending Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.Pending || 0
+                )}
+                color="bg-violet-500"
+                />
+
+                <InfoCard
+                label="In Progress Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.InProgress || 0
+                )}
+                color="bg-cyan-500"
+                />
+
+                <InfoCard
+                label="Completed Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.Completed || 0
+                )}
+                color="bg-lime-500"
+                />
+            </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
+            <div className="md:col-span-2">
+                <div className="card">
+                    <div className="flex items-center justify-between ">
+                        <h5 className="text-lg">Recent Tasks</h5>
+
+                        <button className="card-btn" onClick={onSeeMore}>
+                            See All <LuArrowRight className="text-base" />
+                        </button>
+                    </div>
+
+                    <TaskListTable tableData={dashboardData?.recentTasks || []} />
                 </div>
             </div>
         </div>
