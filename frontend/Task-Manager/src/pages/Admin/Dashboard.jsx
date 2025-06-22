b571f8e5ef013b308d22dba729376bc08a783dae
Arti Thakur <arti31thakur@gmail.com>
2025-06-22 20:26:13 +0530

    Code

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -10,6 +10,9 @@ import { addThousandsSeparator } from "../../utils/helper";
 import InfoCard from "../../components/cards/InfoCard";
 import { LuArrowRight } from "react-icons/lu";
 import TaskListTable from "../../components/TaskListTable";
+import CustomPieChart from "../../components/Charts/CustomPieChart";
+
+const COLORS = ["#8D51FF", "#00B8D8", "#7BCE00"];
 
 
 const Dashboard = () => {
@@ -23,6 +26,28 @@ const Dashboard = () => {
     const [pieChartData, setPieChartData] = useState([]);
     const [barChartData, setBarChartData] = useState([]);
 
+    //prepare chart data
+    const prepareChartData = (data) => {
+        const taskDistribution = data?.taskDistribution || null;
+        const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+        const taskDistributionData = [
+            {status: "Pending", count: taskDistribution?.Pending || 0},
+            {status: "In Progress", count: taskDistribution?.InProgress || 0},
+            {status: "Completed", count: taskDistribution?.Completed || 0}, 
+        ];
+
+        setPieChartData(taskDistributionData);
+
+        const PriorityLevelData = [
+            {priority: "Low", count: taskPriorityLevels?.Low || 0},
+            {priority: "Medium", count: taskPriorityLevels?.Medium || 0},
+            {priority: "High", count: taskPriorityLevels?.High || 0},
+        ];
+
+        setBarChartData(PriorityLevelData);
+    };
+
     const getDashboardData = async () => {
         try {
             const response = await axiosInstance.get(
@@ -33,6 +58,7 @@ const Dashboard = () => {
         console.log("✅ Dashboard data from API:", response.data);
             if (response.data) {
                 setDashboardData(response.data);
+                prepareChartData(response.data?.charts || null);
             }
         } catch (error) {
             console.error("Error fetching users:", error);
@@ -96,6 +122,20 @@ const Dashboard = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
+
+        <div>
+            <div className="card">
+                <div className="flex items-center justify-between">
+                    <h5 className="font-medium">Task Distribution</h5>
+                </div>
+
+                <CustomPieChart
+                data={pieChartData}
+                colors={COLORS}
+                />
+            </div>
+        </div>
+
             <div className="md:col-span-2">
                 <div className="card">
                     <div className="flex items-center justify-between ">
